ae81c575acd9c620279f66fb59117f69bd5c7631
Bokume2 <nagatomoyuu2@gmail.com>
2024-11-29 14:43:47 +0900

    create main class

---
+++ src/java/main/com/xxvlang/XenialXmasVigor.java
@@ -0,0 +1,11 @@
+package com.xxvlang;
+
+import java.nio.file.Path;
+import java.nio.file.Files;
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class XenialXmasVigor {
+    public static void main(String args[]) {}
+}
