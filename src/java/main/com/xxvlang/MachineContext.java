b049205de35ed2b17b768f554ccd13318752b4c3
Bokume2 <nagatomoyuu2@gmail.com>
2024-12-15 00:11:29 +0900

    implement MachineContext.connect()

--- src/java/main/com/xxvlang/MachineContext.java
+++ src/java/main/com/xxvlang/MachineContext.java
@@ -1,7 +1,23 @@
 package com.xxvlang;
 
+import java.util.ArrayList;
+
+import com.xxvlang.statement.Statement;
+import com.xxvlang.data.*;
+import com.xxvlang.exception.*;
+
 public class MachineContext {
+
+    private ArrayList<Statement> program;
+    private XXVTrees trees;
+
+    public void connect(Statement statement, XXVTrees trees) throws XXVException {
+        trees.connect(statement.target(),statement.subject());
+    }
     
-    private MachineContext() {}
+    public MachineContext(ArrayList<Statement> program) {
+        this.program = program;
+        this.trees = new XXVTrees();
+    }
 
 }
